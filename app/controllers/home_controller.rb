40e85e80582278f04d6f8a9ede3aa0d21329e952
Robert Kubica <robert.kubica1982@gmail.com>
2021-09-08 15:33:24 +0200

    Devise added and layout corrected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @recipes = Recipe.all
+  end
+
+  # GET /recipes/1 or /recipes/1.json
+  def show
+  end
+
+  # GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
+
+  # GET /recipes/1/edit
+  def edit
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipes
   resources :ingredients
-  resources :dishes
   #get 'home/index'
   get 'home/about'
   root 'home#index'
