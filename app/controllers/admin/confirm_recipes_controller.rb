815f3aa6d496d96734e2b85b0f26fa98d3b1ca29
Robert Kubica <robert.kubica1982@gmail.com>
2021-12-13 13:50:25 +0100

    Adding Destroy method to Admin Panel

--- app/controllers/admin/confirm_recipes_controller.rb
+++ app/controllers/admin/confirm_recipes_controller.rb
@@ -6,12 +6,12 @@ module Admin
 
     def create
       @recipe = Recipe.find(params[:id]).update(accept: true)
-
       redirect_to admin_confirm_recipes_path, notice: "Recipe accepted"
     end
 
     def destroy
-
+      @recipe = Recipe.find(params[:id]).destroy
+      redirect_to admin_confirm_recipes_path, notice: "Recipe deleted"
     end
   end
 end
