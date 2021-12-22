3c299f56cf41f098540c304b267f77d7a72f92a9
Robert Kubica <robert.kubica1982@gmail.com>
2021-12-22 13:14:02 +0100

    Adding sorting to searching view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @q = Recipe.all.where(accept: true).ransack(params[:q])
+    @q = Recipe.all.where(accept: true).order(name: :desc).ransack(params[:q])
     @recipes = @q.result(distinct: true)
     @recipes_last5 = Recipe.all.where(accept: true).last(5)
     @current_user = current_user
