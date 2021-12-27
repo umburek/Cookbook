b6c3f17027835ebdad491d91a8131f7d262deb61
Robert Kubica <robert.kubica1982@gmail.com>
2021-12-27 19:46:09 +0100

    Update for Policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authorize_user
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -11,10 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
-  def authorize_user
-
-  end
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
